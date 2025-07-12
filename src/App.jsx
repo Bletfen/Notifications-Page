581bb9f0e5e7fbe88c85c43f14b17d8359edf0f2
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-12 21:42:00 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import NotiPage from "./components/NotiPage";
+function App() {
+  return (
+    <>
+      <NotiPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NotiPage.jsx
@@ -0,0 +1,61 @@
+import "./notipage.css";
+import data from "../data.json";
+export default function NotiPage() {
+  return (
+    <div className="notifications-container">
+      <div className="notification-title-read-wrapper">
+        <div className="notification-title">
+          <h1>Notifications</h1>
+          <div className="notification-numbers">3</div>
+        </div>
+        <div className="mark-all">
+          <span className="mark-all-text">Mark all as read</span>
+        </div>
+      </div>
+      {data.map((notification) => (
+        <div className="notifications-wrapper">
+          <div className="profile-img">
+            <img src={notification.profilePic} alt="profile-pic" />
+          </div>
+          <div className="type-of-noti">
+            <div className="post-description">
+              <span
+                className={`user-name ${
+                  notification.isRead === false ? "active" : ""
+                }`}
+              >
+                {notification.username}
+              </span>
+              <span className="action">{notification.action}</span>
+              <span className="group-name">{notification.groupName}</span>
+              <span className="post">{notification.post}</span>
+              {notification.isRead === false ? (
+                <svg
+                  width="8"
+                  height="8"
+                  viewBox="0 0 8 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle cx="4" cy="4" r="4" fill="#F65552" />
+                </svg>
+              ) : (
+                ""
+              )}
+            </div>
+            <div className="time">
+              <span className="time-text">{notification.time}</span>
+              {notification.text ? (
+                <div className="text-wrapper">
+                  <span className="text">{notification.text}</span>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
